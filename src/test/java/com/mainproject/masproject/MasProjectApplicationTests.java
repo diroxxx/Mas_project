e6fd56ad966f984393c7212f056cb8ce123cda77
diroxxx <che.dom29@gmail.com>
2025-05-28 09:13:09 +0200

    Initialize Spring Boot project
    Set up the base Spring Boot project structure with Maven, including necessary dependencies, sample configurations, and main application entry point. Added `.gitignore`, Maven wrapper files, and a basic test class.

---
+++ src/test/java/com/mainproject/masproject/MasProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainproject.masproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
